ba3b383bcf6e6388b028053b369edc97ec0d2112
Shihab Shahriar Khan <redoykhan555@gmail.com>
2019-12-12 19:45:32 +0600

    First complete run; analysis little completed

--- library/utils.py
+++ library/utils.py
@@ -33,22 +33,24 @@ def read_data(file,stats=True):
     if stats:
         noise = (df.HeuBug!=df.RealBug).sum()/len(df)
         imb = np.unique(y_noisy,return_counts=True)[1]
-        print(f"noise:{noise:.3f}, imb:{imb.max()/imb.min():.3f},{imb.min()},{imb.max()}, Shape:{X.shape}")
+        print(f"{file} noise:{noise:.3f}, imb:{imb.max()/imb.min():.3f},{imb.min()},{imb.max()}, Shape:{X.shape}")
     return X,y_noisy,y_real
 
 NEED_PROBS = []
 def evaluate(clf,X,y_noisy,y_real,cv,scorers):
     scores = defaultdict(list)
-    for train_id, test_id in cv.split(X,y_noisy):
+    for train_id, test_id in cv.split(X,y_real):  #vs y_noisy, to solve no-pos-label-in-test-set bug
         clf = clf.fit(X[train_id],y_noisy[train_id])
         probs = clf.predict_proba(X[test_id])
         labels = np.argmax(probs,axis=1)
         for func in scorers:
+            yp = probs[:,1]
             try:
                 func([0,1,1],[.2,.6,.7])
-                scores[func.__name__].append(func(y_real[test_id],probs[:,1]))
+                yp = probs[:,1]
             except ValueError as e:
-                scores[func.__name__].append(func(y_real[test_id],labels))
+                yp = labels
+            scores[func.__name__].append(func(y_real[test_id],yp))
     for func in scorers:
         scores[func.__name__] = np.array(scores[func.__name__])
     return scores
